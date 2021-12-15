871881959673adf7c4f21260e3c468ef88e43cfb
kwokronny <green68599@icloud.com>
2021-12-16 00:07:17 +0800

    upd: 完成 FormAuto Prop 相关单元测试

--- tests/specs/form-auto.spec.js
+++ tests/specs/form-auto.spec.js
@@ -1,9 +1,34 @@
 import { expect } from "chai";
 import FormAuto from "../../packages/FormAuto";
-import { createTest } from "../util";
+import { createTest, createVue } from "../util";
 
 describe("FormAuto", () => {
-  it("props:label hidden", (done) => {
+  it("props: inline", (done) => {
+    let vm = createTest(
+      FormAuto,
+      {
+        inline: true,
+        data: {
+          field1: {
+            label: "field1",
+            type: "text",
+          },
+          field2: {
+            label: "field2",
+            type: "text",
+          },
+        },
+      },
+      true
+    );
+    let form = vm.$el;
+    expect(form.className.indexOf("el-form--inline") > -1).to.be.true;
+    expect(form.querySelector("span.el-form-auto-row").tagName).to.equal("SPAN");
+    expect(form.querySelectorAll("span.el-form-auto-row .el-col").length).to.equal(0);
+    done();
+  });
+
+  it("props:label-hidden", (done) => {
     let vm = createTest(
       FormAuto,
       {
@@ -17,17 +42,19 @@ describe("FormAuto", () => {
             label: "field2",
             type: "text",
           },
-          field2: {
+          field3: {
             labelHidden: false,
-            label: "field2",
+            label: "field3",
             type: "text",
           },
         },
       },
       true
     );
-    let hidden = vm.$el.querySelectorAll(".el-form-item__label");
-    expect(hidden.length == 0).to.be.true;
+    let formItems = vm.$el.querySelectorAll(".el-form-item");
+    formItems.forEach((item) => {
+      expect(item.querySelector("el-form-item__label")).to.be.null;
+    });
     done();
   });
 
@@ -60,5 +87,30 @@ describe("FormAuto", () => {
     expect(label[2].style.width).to.equal("120px");
     done();
   });
-  // hidden is create
+
+  it("props:gutter", (done) => {
+    let vm = createTest(FormAuto, {
+      gutter: 20,
+      data: {
+        field1: {
+          col: 12,
+          label: "field1",
+          type: "text",
+        },
+        field2: {
+          col: 12,
+          label: "field2",
+          type: "text",
+        },
+      },
+    });
+    let row = vm.$el.querySelector(".el-form-auto-row.el-row.el-row--flex");
+    let cols = row.querySelectorAll(".el-col");
+    cols.forEach((col) => {
+      expect(col.style.paddingRight == "10px" && col.style.paddingLeft == "10px").to.be.true;
+    });
+    done();
+  });
+
+
 });
